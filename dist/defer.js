74e713ab9ed9c70a78f905b2194796b513035f13
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-02-23 15:27:54 -0600

    New version: added globals-based versions of modules.

--- dist/defer.js
+++ dist/defer.js
@@ -1,78 +1,90 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.heya || (g.heya = {})).defer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function () {
-    'use strict';
-    var queue = [], backlog = [], reads = [], writes = [], handle = null, start, PROCESSING = {};
-    function processJobs() {
-        handle = PROCESSING;
-        for (var i = 0; i < reads.length; ++i) {
-            reads[i]();
-        }
-        reads = [];
-        for (i = 0; i < queue.length; ++i) {
-            queue[i]();
-        }
-        queue = backlog;
-        backlog = [];
-        handle = null;
-        for (i = 0; i < writes.length; ++i) {
-            writes[i]();
-        }
-        writes = [];
-        if (queue.length || reads.length) {
-            start();
-        }
-    }
-    function asap(f) {
-        queue.push(f);
-        if (handle === null) {
-            start();
-        }
-    }
-    function nextTick(f) {
-        (handle === PROCESSING ? backlog : queue).push(f);
-        if (handle === null) {
-            start();
-        }
-    }
-    function submitRead(f) {
-        reads.push(f);
-        if (handle === null) {
-            start();
-        }
-    }
-    function submitWrite(f) {
-        writes.push(f);
-        if (handle === null) {
-            start();
-        }
-    }
-    if (typeof setImmediate == 'function') {
-        start = function () {
-            handle = setImmediate(processJobs);
-        };
-    } else if (typeof postMessage == 'function' && typeof addEventListener == 'function') {
-        addEventListener('message', function (evt) {
-            var src = evt.source;
-            if ((src === window || src === null) && evt.data === 'ay-process-tick') {
-                evt.stopPropagation();
-                processJobs();
-            }
-        }, true);
-        start = function () {
-            handle = 1;
-            postMessage('ay-process-tick', '*');
-        };
-    } else {
-        start = function () {
-            handle = setTimeout(processJobs);
-        };
-    }
-    return {
-        asap: asap,
-        nextTick: nextTick,
-        submitRead: submitRead,
-        submitWrite: submitWrite
-    };
-}();
-},{}]},{},[1])(1)
+(function(_,f,g){g=window;g=g.heya||(g.heya={});g.defer=f();})
+([], function () {
+	'use strict';
+
+	// general job queue, with dedicated read/write slots partially inspired by https://github.com/wilsonpage/fastdom under MIT
+
+	var queue = [], backlog = [], reads = [], writes = [], handle = null, start,
+		PROCESSING = {}; // handle can be null (nothing), PROCESSING (processing), or something else (delay)
+
+	function processJobs () {
+		handle = PROCESSING;
+		// run reads
+		for (var i = 0; i < reads.length; ++i) {
+			reads[i]();
+		}
+		reads = [];
+		// run regular jobs
+		for (i = 0; i < queue.length; ++i) {
+			queue[i]();
+		}
+		queue = backlog;
+		backlog = [];
+		handle = null;
+		// run writes
+		for (i = 0; i < writes.length; ++i) {
+			writes[i]();
+		}
+		writes = [];
+		if (queue.length || reads.length) {
+			start();
+		}
+	}
+
+	function asap (f) {
+		queue.push(f);
+		if (handle === null) {
+			start();
+		}
+	}
+
+	function nextTick (f) {
+		(handle === PROCESSING ? backlog : queue).push(f);
+		if (handle === null) {
+			start();
+		}
+	}
+
+	function submitRead (f) {
+		reads.push(f);
+		if (handle === null) {
+			start();
+		}
+	}
+
+	function submitWrite (f) {
+		writes.push(f);
+		if (handle === null) {
+			start();
+		}
+	}
+
+	if (typeof setImmediate == 'function') {
+		start = function () {
+			handle = setImmediate(processJobs);
+		};
+	} else if (typeof postMessage == 'function' && typeof addEventListener == 'function') {
+		addEventListener('message', function (evt) {
+			var src = evt.source;
+			if ((src === window || src === null) && evt.data === 'ay-process-tick') {
+				evt.stopPropagation();
+				processJobs();
+			}
+		}, true);
+		start = function () {
+			handle = 1;
+			postMessage('ay-process-tick', '*');
+		};
+	} else {
+		start = function () {
+			handle = setTimeout(processJobs);
+		};
+	}
+
+	return {
+		asap:        asap,        // run possibly in this cycle
+		nextTick:    nextTick,    // run after a delay in the next cycle
+		submitRead:  submitRead,  // run a read operation in a single batch
+		submitWrite: submitWrite  // run a write operation in a single batch
+	};
 });
